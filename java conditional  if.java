c3b8fac17bf4d11034916c6906f542046399199e
Pratheesh20 <43058604+Pratheesh20@users.noreply.github.com>
2018-10-03 12:07:55 +0530

    Update java conditional  if.java

--- java conditional  if.java
+++ java conditional  if.java
@@ -5,15 +5,22 @@ class GUVI
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	String name="Ahana";
-        String city="Chennai";
-        int age=21;
-        int height=5.1;
-        boolean married=true;
-        char sex='F';
-        double city_population=4828853.00;
+	int marksObtained, passingMarks;
  
-        System.out.println("Name:" +name + "\n City:" +city + "\n Age:" +age + "\n Height:"+height +"\n Married:" +married +"\n Sex:" +sex +"\n City Population:" + city_population);// your code goes here
-	}
+    passingMarks <=50;
+ 
+    Scanner input = new Scanner(System.in);
+ 
+    System.out.println("Input marks scored by you");
+ 
+    marksObtained = input.nextInt();
+ 
+    if (marksObtained >= passingMarks) {
+      System.out.println("more than or equal to 50");
+    }
+    else {
+      System.out.println("flop");
+    }
+
 	}
 }
