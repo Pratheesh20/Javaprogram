a3a5e9392a0f1bd14b10552e8fe9644e230446a7
Pratheesh20 <43058604+Pratheesh20@users.noreply.github.com>
2018-10-03 12:10:53 +0530

    Update java conditional if else ladder.java

--- java conditional if else ladder.java
+++ java conditional if else ladder.java
@@ -5,15 +5,26 @@ class GUVI
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	String name="Ahana";
-        String city="Chennai";
-        int age=21;
-        int height=5.1;
-        boolean married=true;
-        char sex='F';
-        double city_population=4828853.00;
+		
+public static void main(String args[])
+	{
  
-        System.out.println("Name:" +name + "\n City:" +city + "\n Age:" +age + "\n Height:"+height +"\n Married:" +married +"\n Sex:" +sex +"\n City Population:" + city_population);// your code goes here
-	}
+	double  dis,amount,markedprice,s;
+               
+	markedprice=1000;
+ 
+        dis=25;  // 25 mean 25%			
+	
+	System.out.println("markedprice= "+markedprice);
+ 
+	System.out.println("discount rate="+dis);
+         
+        s=100-dis;
+ 
+	amount= (s*markedprice)/100;
+ 
+	System.out.println("amount after discount="+amount);
+  }
+}
 	}
 }
