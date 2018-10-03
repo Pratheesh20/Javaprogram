f3a7f396ffe9de9eba268fff13f996400b2a3d2d
Pratheesh20 <43058604+Pratheesh20@users.noreply.github.com>
2018-10-03 12:21:08 +0530

    Create java string tokenizer.java

---
+++ java string tokenizer.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GUVI
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+public class String_main {
+	
+	public static void main(String args[]) {
+		
+		String address1 = " Mr.Jack,Door No 21,Mason Street,3rd Main Road,Velachery,Chennai,Pin-600113";
+		String address2 = " Ms Jill,Door No 53,Mason Street,3rd Main Road,Velachery,Chennai,Pin-600113";
+		String address3 = " Mr Holmes,Door No 13,Valmiki Street,32d Main Road,Saidapet,Chennai,Pin-600115";
+		String address = " Mr.Davinci,Door No 21,Mason Street,3rd Main Road,Velachery,Chennai,Pin-600113";
+		
+		String ans1 = "Names in all addresses";
+		String ans2 = "If address are same but names are different";
+		String ans3 = "all addresses in Mason Street";
+		String ans4 = "all addresses have Pincode";
+		
+		System.out.println(ans1);
+		System.out.println(ans2);		
+		System.out.println(ans3);		
+		System.out.println(ans4);		
+	}
+}
+	}
+}
