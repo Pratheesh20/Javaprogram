67ae229364552aa53bb939bdbac88364ec3d2ea0
Pratheesh20 <43058604+Pratheesh20@users.noreply.github.com>
2018-10-04 11:33:07 +0530

    Create java packages.java

---
+++ java packages.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GUVI
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	mport java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+System.out.println("after copying sign from -10, the value is:"+Math.copySign(10.5,-10));
+System.out.println("after copying sign from -5, the value is:"+Math.copySign(10.5,-5));
+}
+}
+
+	}
+}
