ec32f7b3361ff14640d9d4303a842b0f9bdbffd7
Pratheesh20 <43058604+Pratheesh20@users.noreply.github.com>
2018-10-05 12:29:23 +0530

    Create sumnatural.java

---
+++ sumnatural.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.Scanner;
+public class sumnatural{
+    public static void main(String[] args) {
+        int num,sum=0;
+       Scanner in=new Scanner(System.in);
+       num = in.nextInt();
+        for(int i = 1; i <= num; ++i)
+        {
+        sum += i;
+        }
+        System.out.println(+ sum);
+    }}
